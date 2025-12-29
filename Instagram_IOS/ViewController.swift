a6f4b4551522cb79ba60ac261df542762abe0b86
Pranathi Voora <pranathivoora2@gmail.com>
2025-12-29 09:45:53 +0530

    Initial Commit

---
+++ Instagram_IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram_IOS
+//
+//  Created by Pranathi Voora on 12/29/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
