dc0ee7e97b38efaa958644f240f90ec8e24f2ca3
Pranathi Voora <pranathivoora2@gmail.com>
2026-01-12 09:00:05 +0530

    adding progress login ui

--- Instagram_IOS/Controllers/Other/Onboarding/LoginViewController.swift
+++ Instagram_IOS/Controllers/Other/Onboarding/LoginViewController.swift
@@ -9,13 +9,37 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    struct Constants {
+        static let cornerRadius: CGFloat = 8.0
+        
+    }
+    
     private let usernameEmailField: UITextField = {
-        return UITextField()
+        let field = UITextField()
+        field.placeholder = "Username or Email..."
+        field.returnKeyType = .next
+        field.leftViewMode = .always
+        field.leftView = UIView(frame: CGRect(x: 0, y:0, width:10, height:0))
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.layer.masksToBounds = true
+        field.layer.cornerRadius = Constants.cornerRadius
+        field.backgroundColor = .secondarySystemBackground
+        return field
     }()
     
     private let passwordField: UITextField = {
         let field = UITextField()
         field.isSecureTextEntry = true
+        field.placeholder = "Password..."
+        field.returnKeyType = .next
+        field.leftViewMode = .always
+        field.leftView = UIView(frame: CGRect(x: 0, y:0, width:10, height:0))
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.layer.masksToBounds = true
+        field.layer.cornerRadius = Constants.cornerRadius
+        field.backgroundColor = .secondarySystemBackground
         return field
     }()
     
@@ -24,7 +48,13 @@ class LoginViewController: UIViewController {
     }()
     
     private let termsButton: UIButton = {
-        return UIButton()
+        let button = UIButton()
+        button.setTitle("Login", for: .normal)
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = Constants.cornerRadius
+        button.backgroundColor = .systemBlue
+        button.setTitleColor(.white, for: .normal)
+        return button
     }()
     
     private let privacyButton: UIButton = {
@@ -32,12 +62,18 @@ class LoginViewController: UIViewController {
     }()
     
     private let createAccountButton: UIButton = {
-        return UIButton()
+        let button = UIButton()
+        button.setTitleColor(.label, for: .normal)
+        button.setTitle("New User? Create Account", for: .normal)
+        return button
     }()
     
     private let headerView: UIView = {
-        return UIView()
-    }()
+        let header = UIView()
+        header.clipsToBounds = true
+        let backgroundImageView = UIImageView(image: UIImage(named: "gradient"))
+        header.addSubview(backgroundImageView)
+        return header    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -50,6 +86,23 @@ class LoginViewController: UIViewController {
         super.viewDidLayoutSubviews()
         
         //assign frames
+        headerView.frame = CGRect(x:0,
+                                  y: view.safeAreaInsets.top,
+                                  width: view.width,
+                                  height: view.height/3.0)
+        
+        configureHeaderView()
+    }
+    
+    private func configureHeaderView() {
+        guard headerView.subviews.count == 1 else {
+            return
+        }
+        
+        guard var backgroundView = headerView.subviews.first else {
+            return
+        }
+        backgroundView.frame = headerView.bounds
     }
     
     
